0b248f1797f4bdbb4c8330b79428180dd9465c4f
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2023-09-09 17:48:24 +0700

    Complete task with tests

---
+++ task_1_1_1/src/main/java/org/example/Main.java
@@ -0,0 +1,63 @@
+package org.example;
+
+class HeapSorter {
+    private int[] arr;
+    private int heapSize;
+
+    private void swap(int a, int b) {
+        int tmp = this.arr[a];
+        this.arr[a] = this.arr[b];
+        this.arr[b] = tmp;
+    }
+
+    private void siftDown(int i) {
+        int index = i, left = i*2 + 1, swapCandidateIndex;
+        while (left < this.heapSize) {
+            swapCandidateIndex = index;
+            if (this.arr[left] > this.arr[index])
+                swapCandidateIndex = left;
+            if (left + 1 < this.heapSize &&
+                    this.arr[left + 1] > this.arr[swapCandidateIndex])
+                swapCandidateIndex = left + 1;
+            if (swapCandidateIndex != index) {
+                swap(swapCandidateIndex, index);
+                index = swapCandidateIndex;
+                left = index*2 + 1;
+            }
+            else
+                break;
+        }
+    }
+
+    private void heapify() {
+        for (int i = this.arr.length / 2 - 1; i >= 0; --i)
+            this.siftDown(i);
+    }
+
+    private void constructSortedArr() {
+        for (int i = 0; i < this.arr.length - 1; ++i) {
+            swap(0, this.heapSize-1);
+            this.heapSize--;
+            siftDown(0);
+        }
+    }
+
+    public void heapsort(int[] arr) {
+        this.arr = arr.clone();
+        this.heapSize = this.arr.length;
+        this.heapify();
+        this.constructSortedArr();
+    }
+
+    public int[] getArr() { return this.arr; }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        HeapSorter hs = new HeapSorter();
+        hs.heapsort(new int[] {4, 8, 1, -5, 6});
+        for (int elem : hs.getArr()) {
+            System.out.printf("%d ", elem);
+        }
+    }
+}
