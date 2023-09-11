c5eec42c6222d3b0ab4dadd25e43900642c0e2fd
KozlovKV <KozlovKV@users.noreply.github.com>
2023-09-11 12:53:39 +0000

    Deploying to gh-pages from @ KozlovKV/OOP@48b349fd698675bb9acd1d20dd3b0974d02d70e3 🚀

--- task_1_1_1/package-search-index.js
+++ task_1_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.example"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.heapsort"}]
--- task_1_1_1/type-search-index.js
+++ task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"HeapSorter"},{"p":"org.example","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.heapsort","l":"HeapSorter"},{"p":"org.heapsort","l":"Main"}]
