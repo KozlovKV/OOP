717b1ddd4ef03396d3f485cfd725603a1ed85c7d
KozlovKV <KozlovKV@users.noreply.github.com>
2023-09-30 09:16:18 +0000

    Deploying to gh-pages from @ KozlovKV/OOP@84fdcd655b4b315b8a2680888e46334b69ef3ff9 🚀

---
+++ task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"kozlov.kirill.tree","l":"Tree.BFSTreeIterator"},{"p":"kozlov.kirill.tree","l":"Tree.DFSTreeIterator"},{"p":"kozlov.kirill.tree","l":"Tree"}]
