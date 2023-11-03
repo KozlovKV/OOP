584bd7cb5e84e3bed184adb9b2621104cb268aeb
KozlovKV <KozlovKV@users.noreply.github.com>
2023-11-03 15:56:32 +0000

    Deploying to gh-pages from @ KozlovKV/OOP@dd8017b921c19fd6e3c9ce30e3a36fe235551d04 🚀

---
+++ task_1_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"kozlov.kirill.finders","l":"Main"},{"p":"kozlov.kirill.finders","l":"SimpleStringFinder"},{"p":"kozlov.kirill.finders","l":"StringFinder"}]
